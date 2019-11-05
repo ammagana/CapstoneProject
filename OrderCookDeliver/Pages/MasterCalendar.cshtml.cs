6f18080708cd66182338a1e8535c52cc5083b1e3
Davin Eugene Thomas (dthmas31) <dthmas31@memphis.edu>
2019-11-05 16:10:02 -0600

    Created MasterCalendar with heading.

---
+++ OrderCookDeliver/Pages/MasterCalendar.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrderCookDeliver.Pages
+{
+    public class MasterCalendarModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
